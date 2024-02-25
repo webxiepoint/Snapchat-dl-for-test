e510a9d56c09a08d01fd8030fa5c42b1a6ec3757
webxiepoint <109460927+webxiepoint@users.noreply.github.com>
2024-02-25 18:09:12 +0530

    Add files via upload

---
+++ tests/test_downlaoder.py
@@ -0,0 +1,37 @@
+"""Tests for `snapchat_dl` package."""
+import os
+import shutil
+import unittest
+
+from requests.exceptions import HTTPError
+
+from snapchat_dl.downloader import download_url
+
+
+def teardown_module(module):
+    shutil.rmtree(".test-data")
+
+
+class Test_downloader(unittest.TestCase):
+    """Tests for `snapchat_dl.downloader.download_url` package."""
+
+    def setUp(self):
+        """Set up test fixtures."""
+        self.test_url = "https://filesamples.com/samples/video/mp4/sample_640x360.mp4"
+        self.test_url404 = "https://google.com/error.html"
+
+    def test_download_url(self):
+        """Test snapchat_dl download_url."""
+        download_url(self.test_url, ".test-data/test_dl_23.mp4", sleep_interval=0)
+
+    def test_empty_download(self):
+        """Test snapchat_dl download_url."""
+        open(".test-data/test_dl_23.mp4", "w").close()
+        download_url(self.test_url, ".test-data/test_dl_23.mp4", sleep_interval=0)
+
+    def test_download_url_raise(self):
+        """Test snapchat_dl download_url with invalid url."""
+        with self.assertRaises(HTTPError):
+            download_url(
+                self.test_url404, ".test-data/test_dl_23.mp4", sleep_interval=0
+            )
---
+++ tests/test_snapchat_dl.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+"""Tests for `snapchat_dl` package."""
+import json
+import os
+import shutil
+import unittest
+from unittest import mock
+
+from snapchat_dl.snapchat_dl import SnapchatDL
+from snapchat_dl.utils import APIResponseError
+from snapchat_dl.utils import NoStoriesFound
+from snapchat_dl.utils import UserNotFoundError
+
+
+def teardown_module(module):
+    shutil.rmtree(".test-data")
+
+
+class TestSnapchat_dl(unittest.TestCase):
+    """Tests for `snapchat_dl` package."""
+
+    def setUp(self):
+        """Set up test fixtures."""
+        self.snapchat_dl = SnapchatDL(
+            limit_story=10, quiet=True, directory_prefix=".test-data", dump_json=True,
+        )
+        self.test_url = "https://filesamples.com/samples/video/mp4/sample_640x360.mp4"
+        self.test_url404 = "https://google.com/error.html"
+        self.username = "invalidusername"
+        self.html = open(
+            "tests/mock_data/invalidusername.html", "r", encoding="utf8"
+        ).read()
+        self.html_api_error = open(
+            "tests/mock_data/api-error.html", "r", encoding="utf8"
+        ).read()
+        self.html_nostories = open(
+            "tests/mock_data/invalidusername-nostories.html", "r", encoding="utf8"
+        ).read()
+
+    def test_class_init(self):
+        """Test snapchat_dl init."""
+        self.assertTrue(self.snapchat_dl)
+
+    def test_invalid_username(self):
+        """Test snapchat_dl Stories are not available."""
+        with self.assertRaises(UserNotFoundError):
+            self.snapchat_dl.download("username")
+
+    @mock.patch("snapchat_dl.snapchat_dl.SnapchatDL._api_response")
+    def test_api_error(self, api_response):
+        """Test snapchat_dl Download."""
+        api_response.return_value = self.html_api_error
+        with self.assertRaises(APIResponseError):
+            self.snapchat_dl.download(self.username)
+
+    @mock.patch("snapchat_dl.snapchat_dl.SnapchatDL._api_response")
+    def test_get_stories(self, api_response):
+        """Test snapchat_dl Download."""
+        api_response.return_value = self.html
+        self.snapchat_dl.download(self.username)
+
+    @mock.patch("snapchat_dl.snapchat_dl.SnapchatDL._api_response")
+    def test_no_stories(self, api_response):
+        """Test snapchat_dl Download."""
+        api_response.return_value = self.html_nostories
+        with self.assertRaises(NoStoriesFound):
+            self.snapchat_dl.download(self.username)
